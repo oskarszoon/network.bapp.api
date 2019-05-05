90a10dcb81dfcce24b41de797dcd3d0279a3ca82
Siggi <siggi@viljive.nl>
2019-05-06 01:49:46 +0200

    Switched to reading Bapps from blockchain

--- imports/api/bapps/bapps.js
+++ imports/api/bapps/bapps.js
@@ -1,105 +1,135 @@
-export const bapps = [
-    {
-        txId: "8060fb9c851664daaa12161fa24f1e48d12209795005a008abfa50c45fb74318",
-        protocolAddress: "19HxigV4QyBv3tHpQVcUEQyq1pzZVdoAut",
-        name: "Bitstagram",
-        description: "Post an image from Bapps directly to the Bitstagram feed",
-        definition: {
-            version: 1,
-            inputFields: [
-                {
-                    id: "image",
-                    type: "image",
-                    required: true,
-                    output: "base64"
-                }
-            ],
-            protocol: [
-                {
-                    dataElement: "image.data",
-                    process: "base64atob"
-                },
-                "${image.type}",
-                "binary",
-                "${image.fileName}"
-            ],
-            template: '<div class="image"><img src="data:${s3};base64,${lb2}" /></div>',
-            encrypt: false,
-            sign: false,
-            website: 'https://bitstagram.bitdb.network',
-            logo: 'https://bitstagram.bitdb.network/bitstagram.png'
-        }
-    },
-    {
-        txId: "fa84be7e9e44dd1fa0ce9f85bf6e22265ba056f35dc23817883cff33af1f5f96",
-        protocolAddress: "19HxigV4QyBv3tHpQVcUEQyq1pzZVdoAut",
-        name: "B protocol",
-        description: "Post any file to the blockchain",
-        definition: {
-            version: 1,
-            inputFields: [
-                {
-                    id: "file",
-                    type: "file",
-                    required: true,
-                    output: "base64"
-                }
-            ],
-            protocol: [
-                {
-                    dataElement: "file.data",
-                    process: "base64atob"
-                },
-                "${file.type}",
-                "binary",
-                "${file.fileName}"
-            ],
-            template: '<div class="file"><a href="b://${h}">${s5}</a></div>',
-            encrypt: false,
-            sign: false,
-            website: 'https://github.com/unwriter/B',
-            logo: 'https://api.bapp.network/images/b/logo.png'
-        }
-    },
-    {
-        txId: "3e79ffb07d71c2248fb2356b67c9cb177a9f21de7a0d9ff3be6a71033b64fa03",
-        protocolAddress: "1Simsqfg7zu7uhgbaZoKjwKgsHCYEssBD",
-        name: "Site Inspector",
-        description: "Create an inspection report for a site",
-        definition: {
-            version: 1,
-            inputFields: [
-                {
-                    id: "name",
-                    type: "text",
-                    description: "Name of the site",
-                    required: true,
-                },
-                {
-                    id: "location",
-                    type: "geo-device",
-                    description: "Location of the site report",
-                },
-                {
-                    id: "report",
-                    type: "text",
-                    description: "Site report",
-                    required: true,
-                },
-                {
-                    id: "files",
-                    type: "image",
-                    multiple: true,
-                    upload: true // uploads files into b:// and references only txId's
-                }
-            ],
-            protocol: [
-                "${JSON.fields}"
-            ],
-            encrypt: [1],
-            sign: [1],
-            website: 'https://inspector.bapps.network',
-            logo: 'https://api.bapp.network/images/inspector/logo.png'
-        }
+export const bapps = [];
+
+/*
+bapps.push({
+    txId: "11614d6c694d1c4d17929c3d4779191f4533e239c84668dd0a05cb3e39563c81",
+    protocolAddress: "19HxigV4QyBv3tHpQVcUEQyq1pzZVdoAut",
+    name: "Bitstagram",
+    description: "Post an image from Bapps directly to the Bitstagram feed",
+    definition: {
+        version: 1,
+        inputFields: [
+            {
+                id: "image",
+                type: "image",
+                required: true,
+                output: "base64"
+            }
+        ],
+        protocol: [
+            {
+                dataElement: "image.data",
+                process: "base64atob"
+            },
+            "${image.type}",
+            "binary",
+            "${image.fileName}"
+        ],
+        template: '<div class="image"><img src="data:${s3};base64,${lb2}" /></div>',
+        encrypt: false,
+        sign: false,
+        website: 'https://bitstagram.bitdb.network',
+        logo: 'https://bitstagram.bitdb.network/bitstagram.png'
     }
-];
+});
+
+bapps.push({
+    txId: "a370e267830e304339b98d3fa32cb809dfad426ed535cd47325ceb272df34204",
+    protocolAddress: "19HxigV4QyBv3tHpQVcUEQyq1pzZVdoAut",
+    name: "B protocol",
+    description: "Post any file to the blockchain",
+    definition: {
+        version: 1,
+        inputFields: [
+            {
+                id: "file",
+                type: "file",
+                required: true,
+                output: "base64"
+            }
+        ],
+        protocol: [
+            {
+                dataElement: "file.data",
+                process: "base64atob"
+            },
+            "${file.type}",
+            "binary",
+            "${file.fileName}"
+        ],
+        template: '<div class="file"><a href="b://${h}">${s5}</a></div>',
+        encrypt: false,
+        sign: false,
+        website: 'https://github.com/unwriter/B',
+        logo: 'https://api.bapp.network/images/b/logo.png'
+    }
+});
+
+bapps.push({
+    txId: "2b766f55becfa47a09b5cd2389b1be975ebe2bed3c2dba932354aa9a7a610d80",
+    protocolAddress: "1Simsqfg7zu7uhgbaZoKjwKgsHCYEssBD",
+    name: "Site Inspector",
+    description: "Create an inspection report for a site",
+    definition: {
+        version: 1,
+        inputFields: [
+            {
+                id: "name",
+                type: "text",
+                description: "Name of the site",
+                required: true,
+            },
+            {
+                id: "location",
+                type: "geo-device",
+                description: "Location of the site report",
+            },
+            {
+                id: "report",
+                type: "text",
+                description: "Site report",
+                required: true,
+            },
+            {
+                id: "files",
+                type: "image",
+                multiple: true,
+                upload: true // uploads files into b:// and references only txId's
+            }
+        ],
+        protocol: [
+            "${JSON.fields}"
+        ],
+        encrypt: [1],
+        sign: [1],
+        website: 'https://inspector.bapps.network',
+        logo: 'https://api.bapp.network/images/inspector/logo.png'
+    }
+});
+
+Meteor.startup(() => {
+    import datapay from 'datapay';
+    _.each(bapps, (bapp) => {
+       const protocol = [
+           '1BappGysvZJ6iE5PgXUnA6S4XsGbWDSodr',
+           bapp.protocolAddress,
+           bapp.name,
+           bapp.description,
+           JSON.stringify(bapp.definition)
+       ];
+
+       console.log(protocol);
+
+       return false;
+       const bappsKey = "5K2oghKVoWhfQhn54agUARM35J7auf95wRYzT47iPTUY3QoPoQj";
+       datapay.send({
+           data: protocol,
+           pay: {
+               key: bappsKey
+           }
+       }, Meteor.bindEnvironment((err, hash) => {
+           console.log('protocol', bapp.protocolAddress, err, hash);
+       }));
+   });
+});
+*/
--- imports/api/bapps/search.js
+++ imports/api/bapps/search.js
@@ -1,6 +1,8 @@
 import { bapps } from './bapps';
+import { Base64 } from 'meteor/base64';
+import { HTTP } from 'meteor/http';
 
-export const searchBapps = function(searchTerm) {
+export const _searchBapps = function(searchTerm) {
     const results = [];
     const regex = new RegExp(searchTerm, 'i');
 
@@ -13,4 +15,48 @@ export const searchBapps = function(searchTerm) {
     });
 
     return results;
+};
+
+export const searchBapps = function(searchTerm) {
+    const query = {
+        v: 3,
+        q: {
+            find: {
+                'out.s1': "1BappGysvZJ6iE5PgXUnA6S4XsGbWDSodr",
+                'out.s3': { $regex: "^" + searchTerm + ".*",  }
+            },
+            skip: 0,
+            limit: 99,
+        }
+    };
+    const bQuery = Base64.encode(JSON.stringify(query));
+    const url = `https://genesis.bitdb.network/q/1FnauZ9aUH2Bex6JzdcV4eNX7oLSSEbxtN/${bQuery}`;
+
+    const response = HTTP.get(url, {
+        headers: {
+            key: '1KJPjd3p8khnWZTkjhDYnywLB2yE1w5BmU',
+        },
+    });
+
+    const bapps = {};
+    if (response && response.data && response.data.c) {
+        _.each(response.data.c, (bapp) => {
+            try {
+                const out = bapp.out[0]; // first output
+                // dedupe by protocol address
+                // TODO: make this more intelligent
+                bapps[out.s2+out.s3] = {
+                    txId: bapp.tx.h,
+                    protocolAddress: out.s2,
+                    name: out.s3,
+                    description: out.s4,
+                    definition: JSON.parse(out.s5)
+                };
+            } catch(e) {
+                console.error(e);
+            }
+        });
+    }
+
+    return _.values(bapps);
 };
